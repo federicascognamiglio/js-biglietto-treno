0c06edac9fb185bccc48b09644a14885468fd4e2
Federica Scognamiglio <federicascognamiglio@Federicas-Laptop.local>
2024-10-28 16:01:18 +0100

    feat: exercise completed

---
+++ js/script.js
@@ -0,0 +1,39 @@
+// DATI
+// Input
+const tripKm = prompt("Quanti chilometri devi percorrere?");
+const passengerAge = prompt("Quanti anni ha il passegero?");
+
+console.log(tripKm, passengerAge);
+
+// ESECUZIONE LOGICA
+// Conversione Dati
+const kmNum = parseInt(tripKm);
+const ageNum = parseInt(passengerAge);
+console.log(kmNum, typeof kmNum, ageNum, typeof ageNum);
+
+const ticketPrice = tripKm * 0.21
+console.log(ticketPrice);
+
+//Espressione Condizionale
+let result;
+if (ageNum < 18) {
+    result = ticketPrice - (ticketPrice * 20 / 100);
+    console.log(result);
+} else if (ageNum > 65) {
+    result = ticketPrice - (ticketPrice * 40 / 100);
+    console.log(result);
+} else {
+    result = ticketPrice
+    console.log(result);
+}
+
+const finalResult = result.toFixed(2);
+console.log(finalResult);
+
+// OUTPUT
+console.log(`Il prezzo del tuo biglietto è di ${finalResult} €`);
+
+
+
+
+
